a89c663db4869062010e4c1860c15f2e22d5d284
MaseDar Dar <mailspecialforspam@mail.ru>
2020-11-25 11:36:37 +0300

    Собрал диалоговые окна (дезигн)

--- TabDemo/TabDemo/Resource.h
+++ TabDemo/TabDemo/Resource.h
@@ -2,17 +2,30 @@
 // Microsoft Visual C++ generated include file.
 // Used by TabDemo.rc
 //
-#define IDD_ABOUTBOX				100
-#define IDR_MAINFRAME				128
-#define IDR_TabDemoTYPE				130
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDR_TabDemoTYPE                 130
+#define IDD_DIALOG1                     312
+#define IDD_DIALOG2                     313
+#define IDD_DIALOG3                     315
+#define IDC_BOLD                        1001
+#define IDC_ITALIC                      1002
+#define IDC_LEFT                        1002
+#define IDC_UNDERLINE                   1003
+#define IDC_CENTER                      1003
+#define IDC_RIGHT                       1004
+#define IDC_VARIABLE                    1005
+#define IDC_FIXED                       1006
+#define IDC_EDIT1                       1007
+#define IDC_SPACING                     1007
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        314
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
